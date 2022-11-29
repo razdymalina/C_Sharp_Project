a64c1fecf769fd3de2c1fbc8e2c53dedcd7cef33
razdymalina nadezhda <razdymalina@yandex.ru>
2022-11-29 23:21:32 +0300

    Task 1_4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу вычисления
+//модуля числа.
+//6 -> 6
+//-82 -> 82
+
+Console.Write("Введите число:");
+var s = Console.ReadLine();
+int N = s == null ? 0 :int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
