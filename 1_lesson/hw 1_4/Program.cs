07f4ae2c3a13fcff7111cf7c0709ac2ac1fa2b60
razdymalina nadezhda <razdymalina@yandex.ru>
2022-11-30 03:21:13 +0300

    hw 1_4

---
+++ 1_lesson/hw 1_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Write a number");
+int a = int.Parse(Console.ReadLine());
+int count = 2;
+
+if(a == 1)
+{
+    Console.WriteLine("Четных чисел нет");
+}   
+while (count <= a)
+{
+    Console.WriteLine($"{count}");
+    count +=2;
+}    
+Console.WriteLine();
