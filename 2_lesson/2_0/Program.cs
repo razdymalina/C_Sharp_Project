d76d30d707f6b070962ba2ac2ccf5f1b39068169
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-02 02:09:12 +0300

    Task 2_0

---
+++ 2_lesson/2_0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное
+//число из отрезка [10, 99] и показывает наибольшую
+//цифру числа.
+
+int Maxnumber(int num)
+{
+    Console.WriteLine(num);
+
+    if (num % 10 > num / 10)
+        return num % 10;
+    else 
+        return num / 10;
+}
+//123456 % 10 -> 6
+//123456 / 10 -> 12345
+
+Console.WriteLine(Maxnumber(new Random().Next(10, 100)));
