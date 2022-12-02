f6147e000ca727b37827fba9eb821044ba7a249a
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-03 01:47:26 +0300

    Task 2_1

---
+++ 2_lesson/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное
+//трехзначное число и на выходе показывает
+//последнюю цифру этого числа.
+
+int Number (int num)
+{
+     Console.WriteLine(num);
+     return num % 10;
+}  
+
+Console.WriteLine(Number(new Random().Next(100, 1000)));
