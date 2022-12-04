c1e57af502290af6159baaa104c6d5da689ecfdf
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-05 01:56:05 +0300

    hw 2_4

---
+++ 2_lesson/hw 2_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру от 1 до 7");
+void Num(int num)
+{
+    string text = "будний";
+
+    if (num == 7 || num == 6) text = "выходной";
+    Console.WriteLine($"{num} -> {text}");
+}
+
+Num(int.Parse(Console.ReadLine()));
