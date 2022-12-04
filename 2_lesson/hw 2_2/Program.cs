390923afb8f4332229a67f379a70cec460228612
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-05 01:12:14 +0300

    hw 2_2

---
+++ 2_lesson/hw 2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого числа.
+//645 -> 65
+//782 -> 72
+//918 -> 98
+
+string Number (int num)
+{
+     Console.WriteLine(num);
+     return $"{num} -> {num / 100}{num % 10}";  
+}  
+
+Console.WriteLine(Number(new Random().Next(100, 999)));
+
