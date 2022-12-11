6f837fda3c46cd9f75314dff371bcac1cfa2886e
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-11 21:39:11 +0300

    Task 3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+void Squares(int num)
+{
+    int i = 1;
+    Console.Write($"{num} -> ");
+
+    while (num >= i)
+    {
+        Console.Write($"{Math.Pow(i, 2)}, ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+Squares(7);
+Squares(13);
+
+
