a45e2baf90bd4f68ae5f7ffeb048494c7696262f
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-11 21:10:55 +0300

    Task 3_2

---
+++ 3_lesson/3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая
+//принимает на вход координаты двух точек и
+//находит расстояние между ними в 2D
+//пространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+double Distance(float x1, float y1, float x2, float y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));    
+}
+
+Console.WriteLine(Distance(3, 6, 2, 1));
+Console.WriteLine(Distance(7, -5, 1, -1));
