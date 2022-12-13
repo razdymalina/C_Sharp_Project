dbe0e5b91779fc1ecb25ca0ee28fb32215b74e4a
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-13 19:08:50 +0300

    hw 4_2

---
+++ 4_lesson/hw 4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int SumNum(int num)
+{
+    int num_sum = 0;
+
+    while (num > 0)
+    {
+        num_sum += num % 10;
+        num /= 10;
+    }
+    return num_sum;
+}
+
+Console.WriteLine(SumNum(int.Parse(Console.ReadLine())));
