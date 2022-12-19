836784d44e5dcea0b6170a1d8456d8e0e4b411c9
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-19 14:46:57 +0300

    Task 5_4

---
+++ 5_lesson/5_4/Program.cs
@@ -0,0 +1,44 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент, второй и предпоследний
+//и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int[] PairsNum(int[] arr)
+{
+    int size = arr.Length;
+    int flex_size = size / 2 + size % 2;
+    int[] new_arr = new int[flex_size];
+
+    for (int i = 0; i < size / 2; i++)
+        new_arr[i] = arr[i] * arr[size - i - 1];
+
+    if (new_arr[flex_size - 1] == 0)
+        new_arr[flex_size - 1] = arr[flex_size - 1];
+    return new_arr;
+}
+
+int[] arr_1 = MassNums(int.Parse(Console.ReadLine()),
+                       int.Parse(Console.ReadLine()),
+                       int.Parse(Console.ReadLine()));
+Print(arr_1);
+int[] arr_1_new = PairsNum(arr_1);
+Print(arr_1_new);
