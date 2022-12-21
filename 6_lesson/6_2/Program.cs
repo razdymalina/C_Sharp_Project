16d903f8f2b20e5caa57573e02badfde2014553b
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-22 01:00:26 +0300

    Task 6_2

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет преобразовывать
+//десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+string Bin(int n)
+{
+    string result = "";
+
+    while (n > 0)
+    {
+        result = n % 2 + result;
+        n /= 2;
+    }
+    return result;
+}
+
+Console.WriteLine(Bin(int.Parse(Console.ReadLine())));
