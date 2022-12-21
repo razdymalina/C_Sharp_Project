828c8b78059a78c3d41adbd2ab53bd66ef8fc4e3
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-21 23:19:41 +0300

    hw 5_1

---
+++ 5_lesson/hw 5_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+
+    return arr;
+}
+
+int EvenCount(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count += 1;
+    }
+    return count;
+}
+
+int[] arr_1 = MassNums(int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(EvenCount(arr_1));
