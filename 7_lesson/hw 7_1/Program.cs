b1ce33e18071c637e94623c9a9ca0c04492d2ffb
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-29 19:45:54 +0300

    Hw 7_1

---
+++ 7_lesson/hw 7_1/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble() * -10, 2);
+    return arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+double[,] arr_1 = MassNums(row, column,
+int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()));
+Print(arr_1);
