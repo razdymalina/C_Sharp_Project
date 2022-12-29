3efe239ccdbdd137bc4abe780b85631e0831c856
razdymalina nadezhda <razdymalina@yandex.ru>
2022-12-29 20:13:17 +0300

    Hw 7_2

--- 7_lesson/hw 7_2/Program.cs
+++ 7_lesson/hw 7_2/Program.cs
@@ -4,3 +4,54 @@
 //5 9 2 3
 //8 4 2 4
 //17 -> такого числа в массиве нет
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+string FindElement(int[,] arr, int f, int s)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    if (f > row || f <= 0 || s > column || s <= 0)
+        return $"{f} {s} -> нет в массиве";
+    return $"arr[{f}, {s}] = {arr[f - 1, s - 1]} -> есть в массиве";
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+
+Console.Write("Введите позицию строки: ");
+int one = int.Parse(Console.ReadLine());
+Console.Write("Введите позицию столбца: ");
+int two = int.Parse(Console.ReadLine());
+
+Console.WriteLine(FindElement(arr_1, one, two));
